a845900e7b00ec0f62bc9ba76b4ee5fcd4b72bac
EC2 Default User <ec2-user@ip-172-31-26-245.ap-northeast-1.compute.internal>
2023-05-13 06:42:47 +0000

    [add]bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,39 @@
+class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  # 投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+  
+  def index
+    @books = Book.find.(params[:id])
+　  @user = User.find(params[:id])
+    @post_images = @user.books 
+  end
+
+  def show
+    @books = Book.find.(params[:id])
+  end
+  
+  def edit
+    @book  = Book.find.(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+  def post_book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+end
